53e93edbea9b5ae78209eb24f11948e1b1011839
Vladimir <vladimirsharapov@logarithmica.ru>
2025-12-21 00:15:29 +0700

    add new fonts

--- laundry_app/src/App.vue
+++ laundry_app/src/App.vue
@@ -6,6 +6,35 @@
 </template>
 
 <style>
+/* Omnes Regular (place font files in /public/fonts) */
+@font-face {
+  font-family: 'Omnes';
+  src:
+    url('/fonts/Omnes-Regular.woff2') format('woff2'),
+    url('/fonts/Omnes-Regular.woff') format('woff'),
+    url('/fonts/Omnes-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+/* Some distributions name the family as "Omnes Regular" */
+@font-face {
+  font-family: 'Omnes Regular';
+  src:
+    url('/fonts/Omnes-Regular.woff2') format('woff2'),
+    url('/fonts/Omnes-Regular.woff') format('woff'),
+    url('/fonts/Omnes-Regular.ttf') format('truetype');
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+
+:root {
+  --app-font-sans: 'Omnes', 'Omnes Regular', 'Avenir Next', Avenir, Nunito, Futura,
+    'Trebuchet MS', 'Segoe UI', 'Helvetica Neue', Arial, sans-serif;
+}
+
 * {
   margin: 0;
   padding: 0;
@@ -14,5 +43,18 @@
 
 html {
   scroll-behavior: smooth;
+}
+
+html,
+body {
+  font-family: var(--app-font-sans);
+  font-weight: 400;
+}
+
+button,
+input,
+textarea,
+select {
+  font: inherit;
 }
 </style>
